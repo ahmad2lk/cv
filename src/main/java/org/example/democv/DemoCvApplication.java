4218cdbfeb17549edc0441e33980d49a8e7611ae
ahmad-syn <110030771+ahmad-syn@users.noreply.github.com>
2025-09-01 13:32:23 +0200

    ini

---
+++ src/main/java/org/example/democv/DemoCvApplication.java
@@ -0,0 +1,13 @@
+package org.example.democv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/democv/DemoCvApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.democv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
